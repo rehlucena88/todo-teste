a6404218d856681ee9b461a387fe7a0e9fba921b
Renato Lucena <reh.lucena88@gmail.com>
2022-05-23 19:55:41 -0300

    criado o projeto

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function Home() {
+  return (
+    <div id="app">
+      <section>
+        <h1>Adicionar Tarefas</h1>
+      </section>
+      <section>
+        <h1>Adicionar Tarefas</h1>
+      </section>
+      <section>
+        <h2>Tarefas</h2>
+      </section>
+
+      <section>
+        <h2>Tarefas</h2>
+      </section>
+    </div>
+  )
+}
